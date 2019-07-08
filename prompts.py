fc708d78592c6f5605ea2f1e27e4ba053c72232a
Antoine-marchais <antoine.marchais@student.ecp.fr>
2019-07-08 11:21:00 -0400

    strip newlines in prompts

--- prompts.py
+++ prompts.py
@@ -4,15 +4,29 @@ def parse(promptList) :
     currentPrompt = ""
     for line in promptList : 
         if not(line[0] in ["\n","\t"]):
-            parsed[currentPromptName] = currentPrompt
+            parsed[currentPromptName] = stripNewLine(currentPrompt)
             currentPrompt = ""
             currentPromptName = "".join(e for e in line if e.isalnum())
         else : 
             if line[0] == "\t":
                 currentPrompt = currentPrompt + line[1:]
             else : currentPrompt = currentPrompt + line
-    parsed[currentPromptName] = currentPrompt
     return parsed
+
+def stripNewLine(msg):
+    pre = False
+    suf = False
+    i = 0
+    newMsg = msg
+    while (not(pre) or not(suf)) and i<len(msg) :
+        if msg[i] != "\n" and not(pre):
+            newMsg = newMsg[i:]
+            pre = True
+        if msg[-(i+1)] != "\n" and not(suf):
+            newMsg = newMsg[:-i]
+            suf = True
+        i+=1
+    return newMsg
 
 class Prompter:
     def __init__(self,filename):
@@ -34,4 +48,4 @@ class Prompter:
     def getPromptList(self):
         return list(self.prompts.keys())
 
-    
+1    
